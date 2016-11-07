66d627eb1f91d9d40120643707aea0de79f3a388
Tom Murphy <tom@turphdesigns.com>
2016-11-07 10:51:26 -0700

    Add UC3b
    Adding in Mobile Voucher use case.

--- js/navigation.js
+++ js/navigation.js
@@ -2,9 +2,9 @@ $(document).ready(function(){
 
 	$("#nav_uc1a").click(function() {
 		$("#uc1a").show("slow");
-		$("#uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b, #uc4a").hide("slow");
+		$("#uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b, #uc3c, #uc4a").hide("slow");
 		$("#nav_uc1a").toggleClass("active");
-		$("#nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc4a").removeClass();
+		$("#nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc3c, #nav_uc4a").removeClass();
 		// Order Confirmation
 		$("#uc1a #confirmationEmail #header").load('includes/header/header.html #confirmation');
 		$("#uc1a #confirmationEmail #intro").load('includes/intro/intro.html #uc1a_oc');
@@ -33,9 +33,9 @@ $(document).ready(function(){
 	});
 	$("#nav_uc1b").click(function() {
 		$("#uc1b").show("slow");
-		$("#uc1a, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b, #uc4a").hide("slow");
+		$("#uc1a, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b, #uc3c, #uc4a").hide("slow");
 		$("#nav_uc1b").toggleClass("active");
-		$("#nav_uc1a, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc4a").removeClass();
+		$("#nav_uc1a, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc3c, #nav_uc4a").removeClass();
 		// Order Confirmation
 		$("#uc1b #confirmationEmail #header").load('includes/header/header.html #confirmation');
 		$("#uc1b #confirmationEmail #intro").load('includes/intro/intro.html #uc1b_oc');
@@ -50,9 +50,9 @@ $(document).ready(function(){
 	});
 	$("#nav_uc2a").click(function() {
 		$("#uc2a").show("slow");
-		$("#uc1a, #uc1b, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b, #uc4a").hide("slow");
+		$("#uc1a, #uc1b, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b, #uc3c, #uc4a").hide("slow");
 		$("#nav_uc2a").toggleClass("active");
-		$("#nav_uc1a, #nav_uc1b, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc4a").removeClass();
+		$("#nav_uc1a, #nav_uc1b, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc3c, #nav_uc4a").removeClass();
 		// Order Confirmation
 		$("#uc2a #confirmationEmail #header").load('includes/header/header.html #confirmation');
 		$("#uc2a #confirmationEmail #intro").load('includes/intro/intro.html #uc2a_oc');
@@ -81,9 +81,9 @@ $(document).ready(function(){
 	});
 	$("#nav_uc2b").click(function() {
 		$("#uc2b").show("slow");
-		$("#uc1a, #uc1b, #uc2a, #uc2c, #uc2d, #uc3a, #uc3b, #uc4a").hide("slow");
+		$("#uc1a, #uc1b, #uc2a, #uc2c, #uc2d, #uc3a, #uc3b, #uc3c, #uc4a").hide("slow");
 		$("#nav_uc2b").toggleClass("active");
-		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc4a").removeClass();
+		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc3c, #nav_uc4a").removeClass();
 		// Order Confirmation
 		$("#uc2b #confirmationEmail #header").load('includes/header/header.html #confirmation');
 		$("#uc2b #confirmationEmail #intro").load('includes/intro/intro.html #uc2b_oc');
@@ -98,9 +98,9 @@ $(document).ready(function(){
 	});
 	$("#nav_uc2c").click(function() {
 		$("#uc2c").show("slow");
-		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2d, #uc3a, #uc3b, #uc4a").hide("slow");
+		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2d, #uc3a, #uc3b, #uc3c, #uc4a").hide("slow");
 		$("#nav_uc2c").toggleClass("active");
-		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc4a").removeClass();
+		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc3c, #nav_uc4a").removeClass();
 		// Order Confirmation
 		$("#uc2c #confirmationEmail #header").load('includes/header/header.html #confirmation');
 		$("#uc2c #confirmationEmail #intro").load('includes/intro/intro.html #uc2c_oc');
@@ -129,9 +129,9 @@ $(document).ready(function(){
 	});
 	$("#nav_uc2d").click(function() {
 		$("#uc2d").show("slow");
-		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc3a, #uc3b, #uc4a").hide("slow");
+		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc3a, #uc3b, #uc3c, #uc4a").hide("slow");
 		$("#nav_uc2d").toggleClass("active");
-		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc3a, #nav_uc3b, #nav_uc4a").removeClass();
+		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc3a, #nav_uc3b, #nav_uc3c, #nav_uc4a").removeClass();
 		// Order Confirmation
 		$("#uc2d #confirmationEmail #header").load('includes/header/header.html #confirmation');
 		$("#uc2d #confirmationEmail #intro").load('includes/intro/intro.html #uc2d_oc');
@@ -146,9 +146,9 @@ $(document).ready(function(){
 	});
 	$("#nav_uc3a").click(function() {
 		$("#uc3a").show("slow");
-		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3b, #uc4a").hide("slow");
+		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3b, #uc3c, #uc4a").hide("slow");
 		$("#nav_uc3a").toggleClass("active");
-		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3b, #nav_uc4a").removeClass();
+		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3b, #nav_uc3c, #nav_uc4a").removeClass();
 		// Order Confirmation
 		$("#uc3a #confirmationEmail #header").load('includes/header/header.html #confirmation');
 		$("#uc3a #confirmationEmail #intro").load('includes/intro/intro.html #uc3a_oc');
@@ -177,26 +177,57 @@ $(document).ready(function(){
 	});
 	$("#nav_uc3b").click(function() {
 		$("#uc3b").show("slow");
-		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc4a").hide("slow");
+		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc3c, #uc4a").hide("slow");
 		$("#nav_uc3b").toggleClass("active");
-		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc4a").removeClass();
+		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3c, #nav_uc4a").removeClass();
 		// Order Confirmation
 		$("#uc3b #confirmationEmail #header").load('includes/header/header.html #confirmation');
-		$("#uc3b #confirmationEmail #intro").load('includes/intro/intro.html #uc3b_oc');
-		$("#uc3b #confirmationEmail #details").load('includes/orderDetails/orderDetails.html #uc3b_oc');
+		$("#uc3b #confirmationEmail #intro").load('includes/intro/intro.html #uc3a_oc');
+		$("#uc3b #confirmationEmail #details").load('includes/orderDetails/orderDetails.html #uc3a_oc');
 		$("#uc3b #confirmationEmail #support").load('includes/support/support.html');
-		$("#uc3b #confirmationEmail #extras").load('includes/extras/extras.html #uc3b_oc');
+		$("#uc3b #confirmationEmail #extras").load('includes/extras/extras.html #uc3a_oc');
 		$("#uc3b #confirmationEmail #closing").load('includes/closing/closing.html');
 		$("#uc3b #confirmationEmail #legal").load('includes/legal/legal.html #confirmation');
 		$("#uc3b #confirmationEmail #footer").load('includes/footer/footer.html');
+		// Voucher Email
+		$("#uc3b #voucherEmail #header").load('includes/header/header.html #voucher');
+		$("#uc3b #voucherEmail #intro").load('includes/intro/intro.html #uc3a_ve');
+		$("#uc3b #voucherEmail #details").load('includes/voucherDetails/voucherDetails.html #uc3a_ve');
+		$("#uc3b #voucherEmail #support").load('includes/support/support.html');
+		$("#uc3b #voucherEmail #extras").load('includes/extras/extras.html #uc1a_ve');
+		$("#uc3b #voucherEmail #closing").load('includes/closing/closing.html');
+		$("#uc3b #voucherEmail #legal").load('includes/legal/legal.html #confirmation');
+		$("#uc3b #voucherEmail #footer").load('includes/footer/footer.html');
+		// Voucher
+		$("#uc3b #voucherDocument #barcode").load('includes/barcode/barcode.html #uc3b_v');
+		$("#uc3b #voucherDocument #details").load('includes/voucher/voucherDetails.html #uc3b_v');
+		$("#uc3b #voucherDocument #support").load('includes/support/support.html');
+		$("#uc3b #voucherDocument #extras").load('includes/extras/extras.html #uc3b_v');
+		$("#uc3b #voucherDocument #legal").load('includes/legal/legal.html #voucherChicago');
+		$("#uc3b #voucherDocument #footer").load('includes/footer/footer.html');
+	});
+	$("#nav_uc3c").click(function() {
+		$("#uc3c").show("slow");
+		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b, #uc4a").hide("slow");
+		$("#nav_uc3c").toggleClass("active");
+		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc4a").removeClass();
+		// Order Confirmation
+		$("#uc3c #confirmationEmail #header").load('includes/header/header.html #confirmation');
+		$("#uc3c #confirmationEmail #intro").load('includes/intro/intro.html #uc3c_oc');
+		$("#uc3c #confirmationEmail #details").load('includes/orderDetails/orderDetails.html #uc3c_oc');
+		$("#uc3c #confirmationEmail #support").load('includes/support/support.html');
+		$("#uc3c #confirmationEmail #extras").load('includes/extras/extras.html #uc3c_oc');
+		$("#uc3c #confirmationEmail #closing").load('includes/closing/closing.html');
+		$("#uc3c #confirmationEmail #legal").load('includes/legal/legal.html #confirmation');
+		$("#uc3c #confirmationEmail #footer").load('includes/footer/footer.html');
 		// Voucher Email - NONE
 		// Voucher - NONE
 	});
 	$("#nav_uc4a").click(function() {
 		$("#uc4a").show("slow");
-		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b").hide("slow");
+		$("#uc1a, #uc1b, #uc2a, #uc2b, #uc2c, #uc2d, #uc3a, #uc3b, #uc3c").hide("slow");
 		$("#nav_uc4a").toggleClass("active");
-		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b").removeClass();
+		$("#nav_uc1a, #nav_uc1b, #nav_uc2a, #nav_uc2b, #nav_uc2c, #nav_uc2d, #nav_uc3a, #nav_uc3b, #nav_uc3c").removeClass();
 		// Order Confirmation
 		$("#uc4a #confirmationEmail #header").load('includes/header/header.html #confirmation');
 		$("#uc4a #confirmationEmail #intro").load('includes/intro/intro.html #uc4a_oc');
